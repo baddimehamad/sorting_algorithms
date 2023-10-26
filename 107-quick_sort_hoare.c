760958a27262f0dd321cc1d0063071c409db782f
Baddi Mohamed <mehamad199523@gmail.com>
2023-10-26 09:04:20 -0700

    0x1B. C - Sorting algorithms & Big O

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,5 +1,20 @@
 #include "sort.h"
 /**
+*swap 
+*@array: array
+*@item1: array
+*@item2: array
+*/
+void swap(int *array, ssize_t item1, ssize_t item2)
+{
+	int tmp;
+
+	tmp = array[item1];
+	array[item1] = array[item2];
+	array[item2] = tmp;
+}
+
+/**
  *hoare_partition
  *@array: array
  *@first: array
@@ -27,32 +42,6 @@ int hoare_partition(int *array, int first, int last, int size)
 	}
 }
 /**
-*swap 
-*@array: array
-*@item1: array
-*@item2: array
-*/
-void swap(int *array, ssize_t item1, ssize_t item2)
-{
-	int tmp;
-
-	tmp = array[item1];
-	array[item1] = array[item2];
-	array[item2] = tmp;
-}
-
-/**
- *quick_sort_hoare 
- *@array: array
- *@size: array
- */
-void quick_sort_hoare(int *array, size_t size)
-{
-	if (!array || size < 2)
-		return;
-	qs(array, 0, size - 1, size);
-}
-/**
  *qs 
  *@array: array
  *@first: array
@@ -69,4 +58,15 @@ void qs(int *array, ssize_t first, ssize_t last, int size)
 		qs(array, first, position - 1, size);
 		qs(array, position, last, size);
 	}
+}
+/**
+ *quick_sort_hoare 
+ *@array: array
+ *@size: array
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+	qs(array, 0, size - 1, size);
 }
